14379daab0e0a08cc238056d9a78aa5948fa520a
Görkem Efe Dersin <dersingrkmefe2003@gmail.com>
2024-10-19 18:29:18 +0300

    demake
    demake

---
+++ StudentManagement/Profiles/AfterMaps/UpdateStudentRequestAfterMap.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using StudentManagement.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataModels = StudentManagement.DataModels;
+
+namespace StudentManagement.Profiles.AfterMaps
+{
+    public class UpdateStudentRequestAfterMap : IMappingAction<UpdateStudentRequest, DataModels.Student>
+    {
+        public void Process(UpdateStudentRequest source, DataModels.Student destination, ResolutionContext context)
+        {
+            destination.Address = new DataModels.Address()
+            {
+                PhysicalAdress = source.PhysicalAdress,
+                PostalAdress = source.PostalAdress
+
+            };
+        }
+    }
+}
